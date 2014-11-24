1ef9dfc629978bef0a6d11fef83b27ae0f92be04
anthonyd <mryesofyes@gmail.com>
2014-11-24 00:43:41 -0500

    Limited dlg tag conversion.

--- src/tml.nim
+++ src/tml.nim
@@ -1,6 +1,11 @@
-import xmltree, htmlparser, sequtils, strutils, strtabs
+import os, xmltree, htmlparser, sequtils, streams, strutils, strtabs
 
-const DIV_STYLE_DEFAULT = "position:absolute;left:$1;top:$2"
+
+const DIV_STYLE_DEFAULT = "position:absolute;left:$1;top:$2;"
+
+
+proc parseHtml*(s: string): PXmlNode =
+  parseHtml(newStringStream(s))
 
 proc attrExists(xmlNode: PXmlNode, attribute: string): bool =
   return xmlNode.attr(attribute) != ""
@@ -13,7 +18,8 @@ proc makeDivFromLocation(x: string, y: string): PXmlNode =
   result = tDiv 
 
 proc wrapInTag(child, father: PXmlNode): PXmlNode = 
-  ## This is the reverse of xmltree.add, which for some reason does not work
+  ## This is the reverse of xmltree.add, which for some reason does not work in
+  ## a certain context.
   result = newXmlTree(father.tag, [child], father.attrs)
 
 proc convertTagGif*(tag: PXmlNode): PXmlNode =
@@ -23,3 +29,25 @@ proc convertTagGif*(tag: PXmlNode): PXmlNode =
     result = (<>img(src=tag.attr("name") & ".gif")) 
   if tag.attrExists("x") or tag.attrExists("y"):
     result = result.wrapInTag(makeDivFromLocation(tag.attr("x"), tag.attr("y")))
+
+proc convertTagDlg*(tag: PXmlNode): PXmlNode =
+  if tag.innerText == "":
+    result = newText("")
+  else:
+    let tableRow = parseHtml("""
+      <tr><td>
+      <center>$1</center>
+      </td></tr>""".unindent % [tag.innerText])
+    var table = newElement("table")
+    table.attrs = newStringTable({"border": "0", "cellpadding": "2", 
+                                  "bgcolor": "white", "width": "300px"})
+    table.add(tableRow)
+    if tag.attrExists("x") or tag.attrExists("y"):
+      table = table.wrapInTag(makeDivFromLocation(tag.attr("x"), tag.attr("y")))
+    result = table
+
+
+when isMainModule:
+  # let fileName = paramStr(1)
+  # let parsedHtml = loadHtml(fileName)
+  
--- tests/convertTest.nim
+++ tests/convertTest.nim
@@ -1,16 +1,47 @@
-import unittest, xmltree, strtabs, tml
+import unittest, xmltree, strutils, sequtils, strtabs, tml
+
+
+proc htmlEqual(a, b: string): bool = 
+  proc withoutNewlines(str: string): string =
+    var newStr: string = ""
+    for c in str:
+      if c notin NewLines: newStr.add(c)
+    result = newStr
+  result = a.withoutNewlines == b.withoutNewlines
+
 
 test "convert empty gif to empty img":
   let gifHtml = <>gif()
   let imgHtml = "<img />" 
-  check ($convertTagGif(gifHtml) == imgHtml) 
+  check htmlEqual($convertTagGif(gifHtml), imgHtml) 
 
 test "convert gif with source to equivalent img":
   let gifHtml = <>gif(name="source")
   let imgHtml = """<img src="source.gif" />"""
-  check ($convertTagGif(gifHtml) == imgHtml) 
+  check htmlEqual($convertTagGif(gifHtml), imgHtml) 
 
 test "convert gif with source and position to equivalent img":
   let gifHtml = <>gif(name="source", x="10", y="20")
-  let imgHtml = """<div style="position:absolute;left:10;top:20"><img src="source.gif" /></div>""" 
-  check ($convertTagGif(gifHtml) == imgHtml) 
+  let imgHtml = """<div style="position:absolute;left:10;top:20;"><img src="source.gif" /></div>""" 
+  check htmlEqual($convertTagGif(gifHtml), imgHtml) 
+
+test "convert empty dlg to nothing":
+  let dlgHtml = <>dlg(newText(""))
+  check ($convertTagDlg(dlgHtml) == "")
+
+test "convert dlg with text":
+  let dlgHtml = <>dlg(newText("Hello!"))
+  let resultHtml = parseHtml("""
+    <table border="0" cellpadding="2" bgcolor="white" width="300px"><tr><td>
+    <center>Hello!</center>
+    </td></tr></table>""".unindent)
+  check htmlEqual($convertTagDlg(dlgHtml), $resultHtml)
+
+test "convert dlg with position":
+  let dlgHtml = <>dlg(x="100", y="200", newText("Hello!"))
+  let resultHtml = parseHtml("""
+    <div style="position:absolute;left:100;top:200;">
+    <table border="0" cellpadding="2" bgcolor="white" width="300px"><tr><td>
+    <center>Hello!</center>
+    </td></tr></table></div>""".unindent)
+  check htmlEqual($convertTagDlg(dlgHtml), $resultHtml)

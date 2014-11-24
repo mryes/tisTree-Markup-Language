4d2790acd37feeee173aa3c527244c4a81874945
anthonyd <mryesofyes@gmail.com>
2014-11-24 18:44:27 -0500

    Converting dlg with font. Plus refactorings and improved test stuff.

--- src/tml.nim
+++ src/tml.nim
@@ -1,50 +1,72 @@
-import os, xmltree, htmlparser, sequtils, streams, strutils, strtabs
-
+import os, xmltree, htmlparser, sequtils, streams, strutils, strtabs, algorithm
 
 const DIV_STYLE_DEFAULT = "position:absolute;left:$1;top:$2;"
+
 
 
 proc parseHtml*(s: string): PXmlNode =
   parseHtml(newStringStream(s))
 
 proc attrExists(xmlNode: PXmlNode, attribute: string): bool =
-  return xmlNode.attr(attribute) != ""
+  xmlNode.attr(attribute) != ""
 
 proc makeDivFromLocation(x: string, y: string): PXmlNode =
   let x = if x != "": x else: "0"
   let y = if y != "": y else: "0"
   var tDiv = newElement("div")
-  tDiv.attrs = newStringTable({"style": DIV_STYLE_DEFAULT % [x, y]}, modeCaseInsensitive)
+  tDiv.attrs = newStringTable({"style": DIV_STYLE_DEFAULT % [x, y]})
   result = tDiv 
 
-proc wrapInTag(child, father: PXmlNode): PXmlNode = 
+proc wrapInTag(child: var PXmlNode, father: PXmlNode): void = 
   ## This is the reverse of xmltree.add, which for some reason does not work in
   ## a certain context.
-  result = newXmlTree(father.tag, [child], father.attrs)
+  child = newXmlTree(father.tag, [child], father.attrs)
 
-proc convertTagGif*(tag: PXmlNode): PXmlNode =
+proc multiWrap(child: PXmlNode, tags: varargs[PXmlNode]): PXmlNode =
+  var varChild = child
+  for t in tags:
+    varChild.wrapInTag(t)
+  result = varChild
+
+proc isPositioned(tag: PXmlNode): bool =
+  tag.attrExists("x") or tag.attrExists("y")
+
+proc setFont(tag: PXmlNode): PXmlNode =
+  <>font(face=tag.attr("font"), newText(tag.innerText)) 
+
+proc makeTable(content: PXmlNode,
+               border: string = "0",
+               cellpadding: string = "2",
+               bgcolor: string = "white",
+               width: string = "300px"): PXmlNode =
+  <>table(border=border, cellpadding=cellpadding, 
+          bgcolor=bgcolor, width=width, content)
+
+
+
+proc convertTagGif*(tag: PXmlNode): PXmlNode {.procvar.} =
   if tag.attrsLen == 0:
     result = <>img()
   else:
     result = (<>img(src=tag.attr("name") & ".gif")) 
-  if tag.attrExists("x") or tag.attrExists("y"):
-    result = result.wrapInTag(makeDivFromLocation(tag.attr("x"), tag.attr("y")))
+  if tag.isPositioned(): 
+    result.wrapInTag(makeDivFromLocation(tag.attr("x"), tag.attr("y")))
 
-proc convertTagDlg*(tag: PXmlNode): PXmlNode =
+proc convertTagDlg*(tag: PXmlNode): PXmlNode {.procvar.} =
   if tag.innerText == "":
     result = newText("")
   else:
-    let tableRow = parseHtml("""
-      <tr><td>
-      <center>$1</center>
-      </td></tr>""".unindent % [tag.innerText])
-    var table = newElement("table")
-    table.attrs = newStringTable({"border": "0", "cellpadding": "2", 
-                                  "bgcolor": "white", "width": "300px"})
-    table.add(tableRow)
-    if tag.attrExists("x") or tag.attrExists("y"):
-      table = table.wrapInTag(makeDivFromLocation(tag.attr("x"), tag.attr("y")))
+    let centeredTR = [<>center(), <>td(), <>tr()]
+    var tableContent : PXmlNode
+    if tag.attrExists("font"):
+      tableContent = multiWrap(setFont(tag), centeredTR)
+    else: 
+      tableContent = multiWrap(newText(tag.innerText), centeredTR) 
+    var table = makeTable(tableContent)
+    if tag.isPositioned(): 
+      table.wrapInTag(makeDivFromLocation(tag.attr("x"), tag.attr("y")))
     result = table
+
 
 
 when isMainModule:
--- tests/convertTest.nim
+++ tests/convertTest.nim
@@ -1,47 +1,86 @@
 import unittest, xmltree, strutils, sequtils, strtabs, tml
 
 
-proc htmlEqual(a, b: string): bool = 
+
+proc testTmlConversion(tml, html: string; conversion: proc(tag: PXmlNode): PXmlNode): bool = 
+  ## Converts string inputs to XML nodes, performs tML-to-HTML, then compares the
+  ## inputs as strings again to see if it worked. (Converting the html to an XML node   
+  ## is necessary because identical nodes will produce identical string output.) 
   proc withoutNewlines(str: string): string =
     var newStr: string = ""
     for c in str:
       if c notin NewLines: newStr.add(c)
-    result = newStr
-  result = a.withoutNewlines == b.withoutNewlines
+    newStr
+  let tmlConverted = parseHtml(tml).conversion()
+  let htmlNode = parseHtml(html)
+  ($tmlConverted).withoutNewlines == ($htmlNode).withoutNewlines
+
+template convTest(title: string; conversion: proc(tag: PXmlNode): PXmlNode; 
+                  testml: string): expr =
+  test title:
+    let splitMls = split(testml.unindent(eatAllIndent=true), "\n\n")
+    let (tml, html) = (splitMls[0], splitMls[1])
+    check testTmlConversion(tml.strip, html.strip, conversion)
 
 
-test "convert empty gif to empty img":
-  let gifHtml = <>gif()
-  let imgHtml = "<img />" 
-  check htmlEqual($convertTagGif(gifHtml), imgHtml) 
 
-test "convert gif with source to equivalent img":
-  let gifHtml = <>gif(name="source")
-  let imgHtml = """<img src="source.gif" />"""
-  check htmlEqual($convertTagGif(gifHtml), imgHtml) 
+convTest "convert empty gif", convertTagGif,
+  """
+  <gif />
 
-test "convert gif with source and position to equivalent img":
-  let gifHtml = <>gif(name="source", x="10", y="20")
-  let imgHtml = """<div style="position:absolute;left:10;top:20;"><img src="source.gif" /></div>""" 
-  check htmlEqual($convertTagGif(gifHtml), imgHtml) 
+  <img />
+  """
+
+convTest "convert gif with name", convertTagGif,
+  """
+  <gif name="source" />
+
+  <img src="source.gif" />
+  """
+
+convTest "convert gif with position", convertTagGif,
+  """
+  <gif name="source" x="10" y="20" />
+
+  <div style="position:absolute;left:10;top:20;">
+    <img src="source.gif" />
+  </div>
+  """
 
 test "convert empty dlg to nothing":
   let dlgHtml = <>dlg(newText(""))
   check ($convertTagDlg(dlgHtml) == "")
 
-test "convert dlg with text":
-  let dlgHtml = <>dlg(newText("Hello!"))
-  let resultHtml = parseHtml("""
-    <table border="0" cellpadding="2" bgcolor="white" width="300px"><tr><td>
+convTest "convert dlg with text", convertTagDlg,
+  """
+  <dlg>Hello!</dlg>
+  
+  <table border="0" cellpadding="2" bgcolor="white" width="300px">
+  <tr><td>
     <center>Hello!</center>
-    </td></tr></table>""".unindent)
-  check htmlEqual($convertTagDlg(dlgHtml), $resultHtml)
+  </td></tr>
+  </table>
+  """
 
-test "convert dlg with position":
-  let dlgHtml = <>dlg(x="100", y="200", newText("Hello!"))
-  let resultHtml = parseHtml("""
-    <div style="position:absolute;left:100;top:200;">
-    <table border="0" cellpadding="2" bgcolor="white" width="300px"><tr><td>
-    <center>Hello!</center>
-    </td></tr></table></div>""".unindent)
-  check htmlEqual($convertTagDlg(dlgHtml), $resultHtml)
+convTest "convert dlg with position", convertTagDlg,
+  """
+  <dlg x="100" y="200">Hello!</dlg>
+
+  <div style="position:absolute;left:100;top:200;">
+    <table border="0" cellpadding="2" bgcolor="white" width="300px">
+    <tr><td>
+      <center>Hello!</center>
+    </td></tr>
+    </table>
+  </div> 
+  """
+
+convTest "convert dlg with font", convertTagDlg,
+  """
+  <dlg font="arial">Hello!</dlg>
+
+  <table border="0" cellpadding="2" bgcolor="white" width="300px">
+  <tr><td>
+    <center><font face="arial">Hello!</font></center>
+  </td></tr></table>
+  """

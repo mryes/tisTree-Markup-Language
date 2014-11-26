2efb323457c05ed0c098d10f27fd260a7a6b5bbf
anthonyd <mryesofyes@gmail.com>
2014-11-26 01:27:44 -0500

    Added gif speed attribute. Some small fixes and cleaning up.

--- tml.nim
+++ tml.nim
@@ -1,4 +1,4 @@
-import os, xmltree, htmlparser, sequtils, streams, strutils, 
+import os, osproc, xmltree, htmlparser, sequtils, streams, strutils, 
        strtabs, algorithm, parseutils, times
 
 
@@ -34,13 +34,14 @@ proc isPositioned(tag: PXmlNode): bool =
   tag.attrExists("x") or tag.attrExists("y")
 
 proc setFont(tag: PXmlNode): PXmlNode =
-  if not tag.attrExists("font"): return newText(tag.innerText)
-  let fontAttr = tag.attr("font").split(' ')
+  if not tag.attrExists("font"): return tag 
+  let fontAttr = tag.attr("font").split(',')
   var font = newElement("font")
   font.attrs = newStringTable()
   font.attrs["face"] = fontAttr[0]
   if fontAttr.len > 1: font.attrs["size"] = fontAttr[1]
-  font.add(newText(tag.innerText))
+  for i in tag.items:
+    font.add(i)
   result = font
 
 proc makeTable(content: PXmlNode, 
@@ -57,8 +58,9 @@ proc makeTable(content: PXmlNode,
           content)
 
 proc getGifTransformationAttrs*(tag: PXmlNode): seq[tuple[key, value: string]] =
-  const transformationAttrs = ["scale", "crop", "flip", 
-                               "hue", "saturation", "brightness"]
+  const transformationAttrs = ["scale", "crop", "flip", "rotate", 
+                               "hue", "saturation", "brightness",
+                               "delay"]
   result = @[] 
   for k, v in tag.attrs.pairs:
     if k in transformationAttrs: result.add((k, v)) 
@@ -78,37 +80,6 @@ proc makeGifFilename*(gifTag: PXmlNode): string =
   for a in getGifTransformationAttrs(gifTag):
     result.add("-" & a.key & a.value.cleanPercents)
   result.add(".gif")
-
-proc generateTransformedGif(gifTag: PXmlNode): void =
-  proc hasOffset(geomString: string): bool =
-    geomString.count({'+', '-'}) > 0
-  proc fileNewerThan(f1, f2: string): bool =
-    f1.getLastModificationTime() > f2.getLastModificationTime()
-  let filename = makeGifFilename(gifTag)
-  let filenameNoTrs = gifTag.attr("name") & ".gif"
-  if existsFile(filename) and fileNewerThan(filename, filenameNoTrs):
-    return
-  let trsAttrs = getGifTransformationAttrs(gifTag)
-  var trsArgs = "" 
-  for a in trsAttrs:
-    case a.key
-    of "scale": 
-      trsArgs.add(" -filter point -resize " & a.value)
-    of "crop":
-      let cropValue = if not a.value.hasOffset(): a.value & "+0+0" else: a.value
-      trsArgs.add(" -crop " & cropValue & " +repage")
-    of "flip":
-      if 'x' in a.value: trsArgs.add(" -flop")
-      if 'y' in a.value: trsArgs.add(" -flip")
-    of "hue", "saturation", "brightness":
-      trsArgs.add(" -colorspace HSL -modulate ")
-      trsArgs.add(if a.key == "brightness": a.value else: "100")
-      trsArgs.add("," & (if a.key == "saturation": a.value else: "100"))
-      trsArgs.add("," & (if a.key == "hue": a.value else: "100"))
-      trsArgs = trsArgs.replace("%", "") 
-  let command = "convert $1 $2 $3" % [filenameNoTrs, trsArgs, filename]
-  echo command
-  discard execShellCmd(command)
 
 
 
@@ -143,14 +114,9 @@ proc conversionFunction(tag: string): proc(tag: PXmlNode): PXmlNode =
     if c[0] == tag: return c[1] 
   return dummyConvert
 
-proc deleteUnusedGeneratedGifs(gifsUsed: seq[PXmlNode]): void = 
-  let usedGifFilenames = gifsUsed.mapIt(string, it.makeGifFilename())
-  for f in walkFiles(GENERATED_GIF_FOLDER & "/*.gif"):
-    if f notin usedGifFilenames:
-       removeFile(f)
-
-proc tmlToHtml(tmlHead: PXmlNode): PXmlNode =
-  var transformTasks : seq[PXmlNode] = @[]
+proc tmlToHtml(tmlHead: PXmlNode): tuple[html: PXmlNode, gifsToTransform: seq[PXmlNode]] =
+  echo "Translating..."
+  var gifsToTransform: seq[PXmlNode] = @[]
   proc buildHtmlTree(tmlTree: PXmlNode): PXmlNode =
     if tmlTree.kind != xnElement: return tmlTree
     result = newXmlTree(tmlTree.tag, [], tmlTree.attrs)
@@ -158,18 +124,73 @@ proc tmlToHtml(tmlHead: PXmlNode): PXmlNode =
       result.add(buildHtmlTree(i))
     result = conversionFunction(result.tag)(result)
     if tmlTree.tag == "gif" and tmlTree.getGifTransformationAttrs.len > 0:
-      transformTasks.add(tmlTree)
+      gifsToTransform.add(tmlTree)
   # Ignore root tag and go straight to its children.
   # (This means you can use a <tml> tag instead of an <html> tag)
-  result = newElement("html")
+  var html = newElement("html")
   for i in tmlHead.items:
-    result.add(buildHtmlTree(i))
-  for tag in transformTasks: 
-    tag.generateTransformedGif()
-  deleteUnusedGeneratedGifs(transformTasks)
+    html.add(buildHtmlTree(i))
+  result = (html, gifsToTransform)
+
+
+
+proc generateGifTransformCommand(gifTag: PXmlNode): string =
+  proc hasOffset(geomString: string): bool =
+    geomString.count({'+', '-'}) > 0
+  proc fileNewerThan(f1, f2: string): bool =
+    f1.getLastModificationTime() > f2.getLastModificationTime()
+  let filename = makeGifFilename(gifTag)
+  let filenameNoTrs = gifTag.attr("name") & ".gif"
+  if existsFile(filename) and fileNewerThan(filename, filenameNoTrs): return ""
+  let trsAttrs = getGifTransformationAttrs(gifTag)
+  var trsArgs = " -filter point -background \"rgba(0,0,0,0)\""
+  for a in trsAttrs:
+    case a.key
+    of "scale": 
+      trsArgs.add(" -resize " & a.value & " +repage")
+    of "crop":
+      let cropValue = if not a.value.hasOffset(): a.value & "+0+0" else: a.value
+      trsArgs.add(" -crop " & cropValue & " +repage")
+    of "flip":
+      if 'x' in a.value: trsArgs.add(" -flop")
+      if 'y' in a.value: trsArgs.add(" -flip")
+    of "rotate":
+      trsArgs.add(" -rotate " & a.value)
+    of "hue", "saturation", "brightness":
+      trsArgs.add(" -colorspace HSL -modulate ")
+      trsArgs.add(if a.key == "brightness": a.value else: "100")
+      trsArgs.add("," & (if a.key == "saturation": a.value else: "100"))
+      trsArgs.add("," & (if a.key == "hue": a.value else: "100"))
+      trsArgs = trsArgs.replace("%", "")
+    of "delay":
+      trsArgs.add(" -set delay " & a.value) 
+  result = "convert $1 $2 $3" % [filenameNoTrs, trsArgs, filename]
+
+proc transformGifs(gifs: seq[PXmlNode]): void =
+  var trsCommands : seq[string] = @[]
+  for g in gifs: 
+    let command = g.generateGifTransformCommand()
+    if command != "": 
+      echo "Generating " & g.makeGifFilename()
+      trsCommands.add(command)
+  discard execProcesses(trsCommands)
+
+proc deleteUnusedGeneratedGifs(gifsUsed: seq[PXmlNode]): void = 
+  let usedGifFilenames = gifsUsed.mapIt(string, it.makeGifFilename())
+  for f in walkFiles(GENERATED_GIF_FOLDER & "/*.gif"):
+    if f notin usedGifFilenames:
+       removeFile(f)
 
 
 
 when isMainModule:
-  let tmlInput = parseHtml(readFile(paramStr(1)))
-  writeFile(paramStr(1) & ".result", $tmlToHtml(tmlInput))
+  let filename = paramStr(1)
+  let tmlInput = parseHtml(readFile(filename))
+  let (htmlOutput, gifsToTransform) = tmlToHtml(tmlInput)
+  transformGifs(gifsToTransform)
+  deleteUnusedGeneratedGifs(gifsToTransform)
+  let fileExtension = filename.splitFile.ext
+  let outputFilename = if fileExtension == ".html": filename & ".result" 
+                       else: filename.split('.')[0] & ".html" 
+  writeFile(outputFilename, $htmlOutput)
+  echo "Done!"
